263ef05247c9d5219478928dd45468cee90dfa07
Deepika Bekal <deepika.bekal2@gmail.com>
2021-03-03 16:48:27 +0400

    shop function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,44 @@ console.log(enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var shop = function(){
+  var playerItem = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice");
+  // use switch to carry out action
+  switch (playerItem) {
+    case "UPGRADE" || "upgrade":
+      if (playerMoney >= 7){
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        // increase attack and decrease money
+        playerMoney = playerMoney - 7;
+        playerAttack = playerAttack + 6;      
+      } else {
+        window.alert("You dont have enough money!");
+      }      
+      break;
+    case "REFILL" || "refill":
+      if (playerMoney >=7){
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        // increase health and decrease money
+        playerMoney = playerMoney - 7;
+        playerHealth = playerHealth + 20; 
+      } else {
+        window.alert("You dont have enough money!");
+      }       
+      break;
+    case "LEAVE" || "leave":
+      window.alert("Leaving the store.");
+      // do nothing, so function will end
+      break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+
+
+} ;
+
 var endGame = function(){
   window.alert("The game has now ended. Let's see how you did.")
 
@@ -118,6 +156,17 @@ var startGame = function(){
   
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
+      // if player is still alive and we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        // ask if player wants to use the store before next round
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+        // if yes, take them to the store() function
+        if (storeConfirm) {
+          shop();
+        }
+      }
+}
     } else {
       break;
     }
@@ -135,15 +184,4 @@ startGame();
 
 
 
-/*
-var shop = function(){
-  var playerItem = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice");
-  if (playerItem === "REFILL" || playerItem === "refill"){
-    playerMoney = playerMoney - 5;
-    playerHealth = playerHealth + 10;
-  } else if (playerItem === "UPGRADE" || playerItem === "upgrade"){
-    playerMoney = playerMoney - 5;
-    playerAttack = playerAttack + 5;
-  } 
 
-} */
